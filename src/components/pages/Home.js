51bf64326439afee548d21a5b8e83ff69fce27be
Matei Tita <matei_dimitrie.tita@stud.acs.upb.ro>
2021-01-11 19:13:50 +0200

    Ceres v0.1

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SatMap from "../map/SatMap";
+
+function Home() {
+    return (
+        <div className="home">
+            <SatMap></SatMap> 
+        </div>
+    );
+}
+
+export default Home;
